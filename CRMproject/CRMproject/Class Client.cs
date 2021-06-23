e82887576bc3144fd1d630e5d46575caaa3cdd7e
Olena Didenko <olenadidenko98@gmail.com>
2021-06-23 15:31:46 +0300

    Added  Guid Id

--- CRMproject/CRMproject/Class Client.cs
+++ CRMproject/CRMproject/Class Client.cs
@@ -1,23 +1,33 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Runtime.InteropServices;
 
 namespace CRMproject
 {
+  
+
     class Client
+  
     {
         public string Name { get; set; }
         public string Email { get; set; }
         public string PhoneNumber { get; set; }
         public string Surname { get; set; }
         public string LastName { get; set; }
-
+        public Guid Id { get; set; }
     }
+
+    
+
+
     class Program
     {
+        
         static void Main(string[] args)
         {
             Client client = new Client();
+          
             Console.WriteLine("Enter your name:");
             string name = Console.ReadLine();
             Console.WriteLine("Enter your last name:");
@@ -28,6 +38,9 @@ namespace CRMproject
             string email = Console.ReadLine();
             Console.WriteLine("Enter your phone number:");
             string pnonenumber = Console.ReadLine();
+            Guid g = Guid.NewGuid();
+            Console.WriteLine(g);
+           
         }
     }
 }
