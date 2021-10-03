c07434381180bc815c8a71485e5d83a68933fd6f
Olena98 <olenadidenko98@gmail.com>
2021-10-03 15:48:35 +0300

    Add new Method Initialize which set value list of clients from xml file

--- CRMproject/CRMproject/DataBase.cs
+++ CRMproject/CRMproject/DataBase.cs
@@ -9,19 +9,32 @@ namespace CRMproject
 {
     class DataBase
     {
-        private static readonly List<Client> Clients = new List<Client>();
+        private static List<Client> clients = new List<Client>();
+
+        public static void Initialize()
+        {
+            clients = ReadXmlFile("C://Users//Olena//clients.xml"); 
+
+
+            foreach (Client c in clients)
+            {
+
+                Console.WriteLine(c.Name);
+            }
+        }
+        
         public void AddClient(Client client)
         {
-            Clients.Add(client);
+            clients.Add(client);
         }
         public static void SaveClient(Client client)
         {
-
+           
             string path = ("C://Users//Olena//clients.xml");
             FileInfo fileInf = new FileInfo(path);
             XmlDocument xDoc = new XmlDocument();
             XmlNode rootElement = null;
-
+          
             try
             {
                 if (fileInf.Exists)
@@ -67,26 +80,52 @@ namespace CRMproject
             rootElement.AppendChild(clientElem);
             xDoc.Save(path);
             ReadXmlFile(path);
-
-        
+       
         }
 
-        public static void ReadXmlFile(string path)
+        public static List<Client> ReadXmlFile(string path)
         {
+          
+            List<Client> clients = new List<Client>();
             var doc = new XmlDocument();
             doc.Load(path);
             var xRoot = doc.DocumentElement;
             foreach (XmlNode xnode in xRoot)
-            {
+            {      
+
                 if (xnode.Attributes.Count > 0)
                 {
-                    XmlNode attr = xnode.Attributes.GetNamedItem("name");
-                    if (attr != null)
-                        Console.WriteLine(attr.Value);
-                }
-            }
+                    Client client = new Client();     
 
-            Console.WriteLine($"Count of users: {Clients.Count}");
+                    XmlNode attrName = xnode.Attributes.GetNamedItem("name");
+                    if (attrName != null)
+                    {
+                        client.Name = attrName.Value;                      
+                    }
+                    XmlNode attrLastname = xnode.Attributes.GetNamedItem("lastname");
+                    if (attrLastname != null)
+                    {
+                        client.LastName = attrLastname.Value;                     
+                    }
+                    XmlNode attrSurname = xnode.Attributes.GetNamedItem("surname");
+                    if (attrSurname != null)
+                    {
+                        client.Surname = attrSurname.Value;                      
+                    }
+                    XmlNode attrEmail = xnode.Attributes.GetNamedItem("email");
+                    if (attrEmail != null)
+                    {
+                        client.Email = attrEmail.Value;                       
+                    }
+                    XmlNode attrPhonenumber = xnode.Attributes.GetNamedItem("phonenumber");
+                    if (attrPhonenumber != null)
+                    {
+                        client.PhoneNumber = attrPhonenumber.Value;                      
+                    }
+                    clients.Add(client);
+                }                                             
+            }
+            return clients;       
         }
     }
 }
